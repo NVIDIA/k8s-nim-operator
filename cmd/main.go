039cf9bbb6585b3419a0c70be8867c42577c9086
Varun Ramachandra Sekar <vsekar@nvidia.com>
2025-01-10 12:48:35 -0800

    nemo-entitystore controller implementation
    Signed-off-by: Varun Ramachandra Sekar <vsekar@nvidia.com>

--- cmd/main.go
+++ cmd/main.go
@@ -210,6 +210,17 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err = controller.NewNemoEntitystoreReconciler(
+		mgr.GetClient(),
+		mgr.GetScheme(),
+		updater,
+		render.NewRenderer("/manifests"),
+		ctrl.Log.WithName("controllers").WithName("NemoEntitystore"),
+	).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "NemoEntitystore")
+		os.Exit(1)
+	}
+
 	// +kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
