f68c89d3fda8b8731d41be9747bf16623ed6ac83
Varun Ramachandra Sekar <vsekar@nvidia.com>
2025-05-12 12:43:17 -0700

    new port validations for nemo microservice CRs
    Signed-off-by: Varun Ramachandra Sekar <vsekar@nvidia.com>

--- api/apps/v1alpha1/nemo_customizer_types.go
+++ api/apps/v1alpha1/nemo_customizer_types.go
@@ -77,9 +77,11 @@ type NemoCustomizerSpec struct {
 	Tolerations  []corev1.Toleration          `json:"tolerations,omitempty"`
 	PodAffinity  *corev1.PodAffinity          `json:"podAffinity,omitempty"`
 	Resources    *corev1.ResourceRequirements `json:"resources,omitempty"`
-	Expose       ExposeV1                     `json:"expose,omitempty"`
-	Scale        Autoscaling                  `json:"scale,omitempty"`
-	Metrics      Metrics                      `json:"metrics,omitempty"`
+	// +kubebuilder:validation:XValidation:rule="!(has(self.service.grpcPort))", message="unsupported field: spec.expose.service.grpcPort"
+	// +kubebuilder:validation:XValidation:rule="!(has(self.service.metricsPort))", message="unsupported field: spec.expose.service.metricsPort"
+	Expose  ExposeV1    `json:"expose,omitempty"`
+	Scale   Autoscaling `json:"scale,omitempty"`
+	Metrics Metrics     `json:"metrics,omitempty"`
 
 	// +kubebuilder:validation:Minimum=1
 	// +kubebuilder:default:=1
