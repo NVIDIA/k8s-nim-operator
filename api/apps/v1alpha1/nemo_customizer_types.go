d88378eaa9d1393ad92cf0de99c410a29839a6e4
Shiva Krishna, Merla <smerla@nvidia.com>
2025-06-16 17:14:58 -0700

    Use pointer types to avoid mising hfSecret fields to be deserialized as empty strings
    Signed-off-by: Shiva Krishna, Merla <smerla@nvidia.com>

--- api/apps/v1alpha1/nemo_customizer_types.go
+++ api/apps/v1alpha1/nemo_customizer_types.go
@@ -192,10 +192,10 @@ type ModelDownloadJobsConfig struct {
 	ImagePullPolicy string `json:"imagePullPolicy,omitempty"`
 
 	// NGCSecret is the secret containing the NGC API key
-	NGCSecret NGCSecret `json:"ngcAPISecret,omitempty"`
+	NGCSecret *NGCSecret `json:"ngcAPISecret,omitempty"`
 
 	// HFSecret is the secret containing the HF_TOKEN key
-	HFSecret HFSecret `json:"hfSecret,omitempty"`
+	HFSecret *HFSecret `json:"hfSecret,omitempty"`
 
 	// Optional security context for the job pods
 	SecurityContext *corev1.PodSecurityContext `json:"securityContext,omitempty"`
