067633927593bb27e9de1c0c8a248e6fa2809b07
Shiva Krishna, Merla <smerla@nvidia.com>
2025-09-29 10:14:47 -0700

    Fix to allow overriding DOCKER_BUILD_PLATFORM_OPTIONS
    Signed-off-by: Shiva Krishna, Merla <smerla@nvidia.com>

--- deployments/container/multi-arch.mk
+++ deployments/container/multi-arch.mk
@@ -13,5 +13,6 @@
 # limitations under the License.
 
 PUSH_ON_BUILD ?= false
-DOCKER_BUILD_OPTIONS = --output=type=image,push=$(PUSH_ON_BUILD) --provenance=false --sbom=false
-DOCKER_BUILD_PLATFORM_OPTIONS = --platform=linux/amd64,linux/arm64
+ATTACH_ATTESTATIONS ?= false
+DOCKER_BUILD_OPTIONS = --output=type=image,push=$(PUSH_ON_BUILD) --provenance=$(ATTACH_ATTESTATIONS) --sbom=$(ATTACH_ATTESTATIONS)
+DOCKER_BUILD_PLATFORM_OPTIONS ?= --platform=linux/amd64,linux/arm64
